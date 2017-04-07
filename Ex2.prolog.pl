b91a95929873b078bb3461342556f48fbadfae88
Luís Fernandes <luis.miguelfernandes@sapo.pt>
2017-04-07 02:50:15 +0100

    Inserção de mais conhecimento e documentação do código

--- Ex2.prolog.pl
+++ Ex2.prolog.pl
@@ -22,12 +22,21 @@
 % ----------------------------------------------------------
 %  Extensão do predicado utente: IdUt, Nome, Idade, Sexo, Morada -> {V, F}
 
-utente( 1, joao, 20, masculino, 'vila verde' ).
-utente( 2, jose, 20, masculino, 'lousada' ).
-utente( 3, josefina, 34, feminino, 'aveiro' ).
-utente( 4, luis, 20, masculino, 'vila das aves' ).
-utente( 5, pedro, 20, masculino, 'felgueiras' ).
+utente( 1, joao, 20, masculino, 'Vila Verde' ).
+utente( 2, jose, 20, masculino, 'Lousada' ).
+utente( 3, josefina, 34, feminino, 'Aveiro' ).
+utente( 4, luis, 20, masculino, 'Vila das Aves' ).
+utente( 5, pedro, 20, masculino, 'Felgueiras' ).
 
+-utente( 17, laura, 24, feminino, 'Faro' ).
+-utente( 34, ricardo, 38, masculino, 'Guarda' ).
+-utente( 72, alberto, 83, masculino, 'Setubal' ).
+
+% Admissão do pressuposto do mundo fechado para o predicado utente
+
+-utente(Id,N,I,S,C) :- 
+    nao(utente(Id,N,I,S,C)),
+    nao(excecao(utente(Id,N,I,S,C))).
 
 % Invariante Estrutural (Alínea 1) e 9))
 % Garantia de unicidade nos Ids dos utentes
@@ -72,7 +81,7 @@ utente( 5, pedro, 20, masculino, 'felgueiras' ).
 %                          N == 1).
 
 % Garantir que não se adicionam exceções a conhecimento perfeito positivo
-%+excecao( utente(Id,N,I,S,M) ) :: ( nao( utente(Id,N,I,S,M) ) ).
+%+excecao( utente(Id,N,I,S,M) ) :: ( nao( utente( Id,N,I,S,M ) ) ).
  
 
 % ----------------------------------------------------------
@@ -84,6 +93,12 @@ cuidadoPrestado( 3, 'Medicina Familiar', 'Centro de Saude de Lousada', 'Lousada'
 cuidadoPrestado( 4, 'Medicina Familiar', 'Centro de Saude de Felgueiras', 'Felgueiras').
 cuidadoPrestado( 5, 'Ginecologia', 'Hospital de Braga', 'Braga').
 cuidadoPrestado( 6, 'Obstetricia', 'Hospital de Braga', 'Braga').
+
+% Inserção de Conhecimento Negativo
+
+-cuidadoPrestado( 30,'Medicina Familiar','Hospital Sao Joao','Porto' ).
+-cuidadoPrestado( 45,'Obstetricia','Hospital do Algarve','Faro' ).
+-cuidadoPrestado( 98,'Ginecologia','Centro de Saude de Felgueiras','Felgueiras' ).
 
 
 % Invariante Estrutural (Alínea 1) e 9))
@@ -114,7 +129,7 @@ cuidadoPrestado( 6, 'Obstetricia', 'Hospital de Braga', 'Braga').
                             N == 1 ).
 
 % Garantir que nao se adicionaa excecoes a conhecimento perfeito positivo
-+excecao( cuidadoPrestado(Id,D,I,C) ) :: nao( -cuidadoPrestado(Id,D,I,C) ).
++excecao( cuidadoPrestado(Id,D,I,C) ) :: ( nao( cuidadoPrestado( Id,D,I,C ) ) ).
 
 % Invariante Referencial
 
@@ -126,13 +141,23 @@ cuidadoPrestado( 6, 'Obstetricia', 'Hospital de Braga', 'Braga').
 % ----------------------------------------------------------
 %  Extensão do predicado atoMedico: Data, IdUt, IdServ, Custo -> {V, F}
 
-
 atoMedico( '14-03-2017', 1, 5, 30 ).
 atoMedico( '12-03-2017', 3, 2, 20 ).
 atoMedico( '13-12-2017', 4, 2, 10 ).
 atoMedico( '13-03-2017', 5, 4, 5 ).
 atoMedico( '14-03-2017', 2, 3, 5 ).
 atoMedico( '04-04-2017', 1, 3, 7 ).
+
+-atoMedico( '31-01-2017', 5, 3, 13).
+-atoMedico( '12-07-2017', 3, 4, 43).
+-atoMedico( '06-01-2018', 2, 1, 59).
+-atoMedico( '24-12-2017', 6, 5, 2).
+
+% Pressuposto Mundo Fechado para o predicado atoMedico
+
+-atoMedico(D,Id,IdS,C) :-
+    nao(atoMedico(D,Id,IdS,C)),
+    nao(excecao(atoMedico(D,Id,IdS,C))).
 
 % Invariante Estrutural (Alínea 1) e 9))
 
@@ -145,16 +170,56 @@ atoMedico( '04-04-2017', 1, 3, 7 ).
                     comprimento( S2,N2 ),
                     N2 == 1 ).
 
-% pressuposto mundo fechado
--atoMedico(D,Id,IdS,C) :-
-    nao(atoMedico(D,Id,IdS,C)),
-    nao(excecao(atoMedico(D,Id,IdS,C))).
-
 % Garantir que nao se adicionaa excecoes a conhecimento perfeito positivo
 +excecao( atoMedico(D,I,Is,C) ) :: nao( -atoMedico(D,I,Is,C) ).
 
 
-% --------------------- Predicados auxiliares ----------------------
+% -------------------- PREDICADOS AUXILIARES EXERCICIO 2 --------------------------
+
+% Extensao do meta-predicado demo: Questao,Resposta -> {V,F,D}
+
+demo(Questao, verdadeiro) :- Questao.
+demo(Questao, falso) :- -Questao.
+demo(Questao, desconhecido) :- nao(Questao),
+                              nao(-Questao).
+
+% Extensao do meta-predicado myDemo: [Queries], Resposta -> {V, F, D} 
+
+myDemo( [], verdadeiro ).
+myDemo( [Q], Resposta ) :- demo( Q, Resposta ).
+myDemo( [Q1, Op | T], Resposta ) :- Op == e,
+                                    demo( Q1, V1 ),
+                                    myDemo( T, V2 ),
+                                    conjuncao( V1, V2, Resposta ).
+myDemo( [Q1, Op | T], Resposta ) :- Op == ou,
+                                    demo( Q1, V1 ),
+                                    myDemo( T, V2 ),
+                                    disjuncao( V1, V2, Resposta ).
+
+
+conjuncao( verdadeiro, verdadeiro, verdadeiro ).
+conjuncao( verdadeiro, desconhecido, desconhecido ).
+conjuncao( desconhecido, verdadeiro, desconhecido ).
+conjuncao( desconhecido, desconhecido, desconhecido ).
+conjuncao( falso, _, falso ).
+conjuncao( _, falso, falso ).
+
+disjuncao( verdadeiro, _, verdadeiro ).
+disjuncao( _, verdadeiro, verdadeiro ).
+disjuncao( falso, falso, falso ).
+disjuncao( falso, desconhecido, desconhecido ).
+disjuncao( desconhecido, falso, desconhecido ).
+disjuncao( desconhecido, desconhecido, desconhecido ).
+
+
+% Extensao do meta-predicado demoLista: [Questao],[Resposta] -> {V,F,D}
+
+demoLista( [],[] ).
+demoLista( [X|L],[R|S] ) :- demo( X,R ),
+                            demoLista( L,S ). 
+
+
+% --------------------- PREDICADOS AUXILIARES ANTERIORES ----------------------
 
 % Predicado solucoes: F, Q, S -> {V,F}
 
@@ -162,14 +227,14 @@ solucoes(T,Q,S) :- Q, assert(tmp(T)), fail.
 solucoes(T,Q,S) :- construir(S, []).
 
 construir(S1, S2) :- retract(tmp(X)),
-    !,
-    construir(S1, [X|S2]).
+                     !,
+                     construir(S1, [X|S2]).
 construir(S, S).
 
 
 comprimento( [], 0 ).
 comprimento( [X|T], R ) :- comprimento(T,N),
-               R is N+1.
+                           R is N+1.
 
 % evolucao: F -> {V,F,D}
 evolucao( F ) :- solucoes(I, +F::I, Li),
@@ -182,7 +247,7 @@ evolucao( F ) :- retract( F ),
 % testar: L -> {V,F,D}
 testar([]).
 testar([I|Li]) :- I,
-    testar(Li).
+                  testar(Li).
 
 
 % involucao: F -> {V,F}
@@ -190,110 +255,89 @@ involucao( F ) :- solucoes(I, -F::I, Li),
                   retract(F),
                   testar(Li).
 involucao( F ) :- assert( F ),
-          !,
-          fail.
+                  !,
+                  fail.
 
 % Extensao do meta-predicado nao: Questao -> {V,F}
 
-nao( Questao ) :-
-    Questao, !, fail.
+nao( Questao ) :- Questao, !, fail.
 nao( Questao ).
-
-% Extensao do meta-predicado demo: Questao,Resposta -> {V,F,D}
-
-demo(Questao,verdadeiro) :-
-    Questao.
-demo(Questao, falso) :-
-    -Questao.
-demo(Questao,desconhecido) :-
-    nao(Questao),
-    nao(-Questao).
-
-% Extensao do meta-predicado myDemo: [Queries], Resposta -> {V, F, D} 
-myDemo( [], verdadeiro ).
-myDemo( [Q], Resposta ) :-
-    demo( Q, Resposta ).
-myDemo( [Q1, Op | T], Resposta ) :-
-    Op == e,
-    demo(Q1, V1),
-    myDemo(T, V2),
-    conjuncao(V1, V2, Resposta).
-myDemo( [Q1, Op | T], Resposta ) :-
-    Op == ou,
-    demo(Q1, V1),
-    myDemo(T, V2),
-    disjuncao(V1, V2, Resposta).
-
-
-conjuncao( verdadeiro,verdadeiro,verdadeiro ).
-conjuncao( verdadeiro,desconhecido,desconhecido ).
-conjuncao( desconhecido,verdadeiro,desconhecido ).
-conjuncao( desconhecido,desconhecido,desconhecido ).
-conjuncao( falso,_,falso ).
-conjuncao( _,falso,falso ).
-
-disjuncao( verdadeiro,_,verdadeiro ).
-disjuncao( _,verdadeiro,verdadeiro ).
-disjuncao( falso,falso,falso ).
-disjuncao( falso,desconhecido,desconhecido ).
-disjuncao( desconhecido,falso,desconhecido ).
-disjuncao( desconhecido,desconhecido,desconhecido ).
-
-
-% Extensao do meta-predicado demoLista: [Questao],[Resposta] -> {V,F,D}
-
-demoLista( [],[] ).
-demoLista( [X|L],[R|S] ) :-
-    demo( X,R ),
-    demoLista( L,S ). 
 
 
 % ----------------------------------------------------------------------
 % ----------------------- Trabalho de Grupo 2 --------------------------
 
-% Admissão do pressuposto do mundo fechado para o predicado utente
-
--utente(Id,N,I,S,C) :- 
-    nao(utente(Id,N,I,S,C)),
-    nao(excecao(utente(Id,N,I,S,C))).
 
 
-% Inserção de Conhecimento Negativo
 
--cuidadoPrestado( Id,'Medicina Familiar','Hospital Sao Joao','Porto' ).
--cuidadoPrestado( Id,'Obstetricia','Hospital do Algarve','Faro' ).
--cuidadoPrestado( Id,'Ginecologia','Centro de Saúde de Felgueiras','Felgueiras' ).
+% ---------------- INSERÇÃO DE CONHECIMENTO IMPERFEITO -----------------
 
-% Inserção de conhecimento imperfeito Incerto
+% ------------------------- Imperfeito Incerto -------------------------
+
+% --- Não se sabe a idade do utente Manuel, cujo IdUt é 6.
 
 utente( 6, manuel, inc0001, masculino, 'Lisboa' ).
 excecao( utente( Id,N,I,S,C ) ) :- utente( Id,N,inc0001,S,C ).
 
-atoMedico( inc0002,3,5,10 ).
+% --- Não se sabe a data em que o utente cujo IdUt é 3 foi submetido ao tratamento cujo IdServ é 5.
+
+atoMedico( inc0002, 3, 5, 10 ).
 excecao( atoMedico( D,IdUt,IdServ,C ) ) :- atoMedico( inc0002,IdUt,IdServ,C ).
 
 
-% Inserção de conhecimento imperfeito Impreciso
 
-excecao(utente( 7,dolores,34,masculino,'Amadora' )).
-excecao(utente( 7,dolores,34,feminino,'Amadora' )).
+% ------------------------- Imperfeito Impreciso -----------------------
 
-excecao(utente( 8,zeca,37,masculino,'Sintra' )).
-excecao(utente( 8,zeca,36,masculino,'Amadora' )).
+% Não se sabe se o/a utente Dolores é do sexo masculino ou feminino.
 
-excecao(utente( 7,'Alfredo',22,masculino,'felgueiras' )).
-excecao(utente( 7,'Alfredo',22,masculino,'lousada' )).
+excecao(utente( 7, dolores, 34, masculino, 'Amadora' )).
+excecao(utente( 7, dolores, 34, feminino, 'Amadora' )).
 
-excecao(utente( 8,'Alzira',23,feminino,'braga' )).
-excecao(utente( 8,'Alzira',24,feminino,'braga' )).
+% Não se sabe se o utente Zeca tem 36 ou 37 anos, nem se é da Amadora ou de Sintra.
 
-% Comentar
+excecao(utente( 8, zeca, 36, masculino, 'Sintra' )).
+excecao(utente( 8, zeca, 37, masculino, 'Sintra' )).
+excecao(utente( 8, zeca, 36, masculino, 'Amadora' )).
+excecao(utente( 8, zeca, 37, masculino, 'Amadora' )).
+
+% Não se sabe se o utente Alfredo é de Felgueiras ou Lousada.
+
+excecao(utente( 7, alfredo, 22, masculino, 'felgueiras' )).
+excecao(utente( 7, alfredo, 22, masculino, 'lousada' )).
+
+% Não se sabe se a utente Alzira tem 23 ou 24 anos.
+
+excecao(utente( 8, alzira, 23, feminino, 'braga' )).
+excecao(utente( 8, alzira, 24, feminino, 'braga' )).
+
+% Não se sabe o preço da consulta que ocorreu no dia 29 de Abril de 2017, cujo utente tem o
+% IdUt 2 e o serviço prestado tem o IdServ 3, mas sabe-se que o preço foi
+% entre os 3 e os 17 euros.
+
 excecao(atoMedico('29-04-2017',2,3,C)) :- C>=3, C=<17.
 
 
-% Invariante que garante a não inserção de excepções repetidas
 
-% Conhecimento positivo
+% ------------------------- Imperfeito Interdito -----------------------
+
+% Não se sabe nem é possível vir a saber a morada do utente António Costa.
+
+utente(10, 'Antonio Costa', 55, masculino, int0001).
+excecao( utente( Id,Nome,Idade,Sexo,Morada ) ) :- utente( Id,Nome,Idade,Sexo,int0001 ).
+nulo( int0001 ).
+
+% Não se sabe nem é possível vir a saber qual o serviço prestado ao utente cujo IdUt é 10
+% no dia 20 de Março de 2017 e cujo preço foi 3000€.
+
+atoMedico( '20-03-2017', 10, int0002, 3000 ).
+excecao( atoMedico( Data,IdUt,IdServ,Custo ) ) :- atoMedico( Data,IdUt,int0002,Custo ).
+nulo( int0002 ).
+
+
+
+% ----------------------------------------------------------------------
+
+% Invariante que garante a não inserção de excepções repetidas
 
 +(excecao(utente(Id,N,I,S,C))) :: (solucoes(excecao(utente(Id,N,I,S,C)),excecao(utente(Id,N,I,S,C)),S),
                     comprimento(S,N),
